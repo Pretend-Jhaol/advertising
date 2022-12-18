33a26106e7386d79206c27e77b5ead958e9d9180
jhaol <ljhao@outlook.com>
2022-12-18 12:33:23 +0800

    主分支测试版

--- main-load.js
+++ main-load.js
@@ -58,31 +58,32 @@ function loadStyle() {
     new_element.setAttribute('rel', 'stylesheet');
     new_element.setAttribute("type", "text/css");
     // new_element.setAttribute('href', 'main-load.css');
-    new_element.setAttribute('href', 'https://cdn.jsdelivr.net/gh/Pretend-Jhaol/advertising@release/main-load.css');
+    new_element.setAttribute('href', 'https://cdn.jsdelivr.net/gh/Pretend-Jhaol/advertising@latest/main-load.css');
     document.head.appendChild(new_element);
 }
+
 //
 loadStyle()
-// addDelayMask("www.baidu.com", 5000)
-// addFloatAdv("right", "bottom", "https://g32554517s.zicp.fun/adv/jump?id=", 'https://fakeimg.pl/220x300/F44336/FFF/?font=noto&text=顶部广告')
-// addFloatAdv("left", "bottom", "https://g32554517s.zicp.fun/adv/jump?id=", 'https://fakeimg.pl/220x300/F44336/FFF/?font=noto&text=顶部广告')
-// addFloatAdv("right", "top", "https://g32554517s.zicp.fun/adv/jump?id=", 'https://fakeimg.pl/220x300/F44336/FFF/?font=noto&text=顶部广告')
-// addFloatAdv("left", "top", "https://g32554517s.zicp.fun/adv/jump?id=", 'https://fakeimg.pl/220x300/F44336/FFF/?font=noto&text=顶部广告')
-// addTopOrBottomAdv("top", "https://g32554517s.zicp.fun/adv/jump?id=", "https://fakeimg.pl/1920x50/F44336/FFF/?font=noto&text=顶部广告")
-// addTopOrBottomAdv("bottom", "https://g32554517s.zicp.fun/adv/jump?id=", "https://fakeimg.pl/1920x50/F44336/FFF/?font=noto&text=顶部广告")
-// var httpRequest = new XMLHttpRequest();
-// httpRequest.open('GET', 'https://g32554517s.zicp.fun/site/config?host=www.zzhifu.com', true);
-// httpRequest.send();
-// httpRequest.onreadystatechange = function () {
-//     if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-//         var json = JSON.parse(httpRequest.responseText);//获取到json字符串，还需解析
-//         console.log(json)
-//         addFloatAdv("right", "bottom", "https://g32554517s.zicp.fun/adv/jump?id=" + json.bottomAdvId, json.bottomImage)
-//         addFloatAdv("left", "bottom", "https://g32554517s.zicp.fun/adv/jump?id=" + json.bottomAdvId, json.bottomImage)
-//         addFloatAdv("right", "top", "https://g32554517s.zicp.fun/adv/jump?id=" + json.topAdvId, json.topImage)
-//         addFloatAdv("left", "top", "https://g32554517s.zicp.fun/adv/jump?id=" + json.topAdvId, json.topImage)
-//         addTopOrBottomAdv("top", "https://g32554517s.zicp.fun/adv/jump?id=" + json.topAdvId, "https://fakeimg.pl/1920x50/F44336/FFF/?font=noto&text=顶部广告")
-//         addTopOrBottomAdv("bottom", "https://g32554517s.zicp.fun/adv/jump?id=" + json.topAdvId, "https://fakeimg.pl/1920x50/F44336/FFF/?font=noto&text=顶部广告")
-//     }
-// };
+addDelayMask("www.baidu.com", 5000)
+addFloatAdv("right", "bottom", "https://g32554517s.zicp.fun/adv/jump?id=", 'https://fakeimg.pl/220x300/F44336/FFF/?font=noto&text=顶部广告')
+addFloatAdv("left", "bottom", "https://g32554517s.zicp.fun/adv/jump?id=", 'https://fakeimg.pl/220x300/F44336/FFF/?font=noto&text=顶部广告')
+addFloatAdv("right", "top", "https://g32554517s.zicp.fun/adv/jump?id=", 'https://fakeimg.pl/220x300/F44336/FFF/?font=noto&text=顶部广告')
+addFloatAdv("left", "top", "https://g32554517s.zicp.fun/adv/jump?id=", 'https://fakeimg.pl/220x300/F44336/FFF/?font=noto&text=顶部广告')
+addTopOrBottomAdv("top", "https://g32554517s.zicp.fun/adv/jump?id=", "https://fakeimg.pl/1920x50/F44336/FFF/?font=noto&text=顶部广告")
+addTopOrBottomAdv("bottom", "https://g32554517s.zicp.fun/adv/jump?id=", "https://fakeimg.pl/1920x50/F44336/FFF/?font=noto&text=顶部广告")
+var httpRequest = new XMLHttpRequest();
+httpRequest.open('GET', 'https://g32554517s.zicp.fun/site/config?host=www.zzhifu.com', true);
+httpRequest.send();
+httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        var json = JSON.parse(httpRequest.responseText);//获取到json字符串，还需解析
+        console.log(json)
+        addFloatAdv("right", "bottom", "https://g32554517s.zicp.fun/adv/jump?id=" + json.bottomAdvId, json.bottomImage)
+        addFloatAdv("left", "bottom", "https://g32554517s.zicp.fun/adv/jump?id=" + json.bottomAdvId, json.bottomImage)
+        addFloatAdv("right", "top", "https://g32554517s.zicp.fun/adv/jump?id=" + json.topAdvId, json.topImage)
+        addFloatAdv("left", "top", "https://g32554517s.zicp.fun/adv/jump?id=" + json.topAdvId, json.topImage)
+        addTopOrBottomAdv("top", "https://g32554517s.zicp.fun/adv/jump?id=" + json.topAdvId, "https://fakeimg.pl/1920x50/F44336/FFF/?font=noto&text=顶部广告")
+        addTopOrBottomAdv("bottom", "https://g32554517s.zicp.fun/adv/jump?id=" + json.topAdvId, "https://fakeimg.pl/1920x50/F44336/FFF/?font=noto&text=顶部广告")
+    }
+};
 
