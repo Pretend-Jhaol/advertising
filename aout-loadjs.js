a9d4efecd6f1c29f0cb8dbf15dc1268be007a02a
jhaol <ljhao@outlook.com>
2022-12-23 12:21:08 +0800

    提交发行版

--- aout-loadjs.js
+++ aout-loadjs.js
@@ -2,6 +2,6 @@
     var _maq = _maq || [];
     _maq.push(['_setAccount', 'HmiSYVGkAJNm']);
     var ma = document.createElement('script'); ma.type = 'text/javascript'; ma.async = true;
-    ma.src = ('https:' == document.location.protocol ? 'https://cdn.jsdelivr.net/gh/Pretend-Jhaol/advertising@latest/' : 'http://cdn.jsdelivr.net/gh/Pretend-Jhaol/advertising@latest/') + 'main-load.js';
+    ma.src = ('https:' == document.location.protocol ? 'https://cdn.jsdelivr.net/gh/Pretend-Jhaol/advertising@latest/' : 'http://cdn.jsdelivr.net/gh/Pretend-Jhaol/advertising@latest/') + 'main-load.min.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ma, s);
 })();
